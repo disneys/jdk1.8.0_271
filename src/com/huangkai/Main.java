5eb99ca376ae0e3c7ae8678e84cb777a3c691862
NIA <nia19881108@gmail.com>
2020-12-18 16:42:52 +0800

    Init

---
+++ src/com/huangkai/Main.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        List l = new ArrayList();
+        Stack s = new Stack();
+
+    }
+}
