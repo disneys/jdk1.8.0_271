534a0a9937bdb0d845116a96d6c1cd7ba5bed45c
NIA <nia19881108@gmail.com>
2020-12-18 16:19:48 +0800

    Init

---
+++ src/com/huangkai/test/hashmap/Test.java
@@ -0,0 +1,17 @@
+package com.huangkai.test.hashmap;
+
+import java.util.HashMap;
+
+public class Test {
+
+    public static void main(String[] args) {
+
+        HashMap<String, Object> hashMap = new HashMap<String, Object>();
+
+        hashMap.put("K1", 0.1);
+        hashMap.put("K2", 0.2);
+        hashMap.put("K3", 0.3);
+        hashMap.put("K4", 0.4);
+    }
+
+}
